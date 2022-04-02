bda8b348112e73262d81853bf7b6816a8447989a
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-04-02 15:49:13 -0400

    Dispatch each sub-command out to a specific function

--- scaler.py
+++ scaler.py
@@ -1,23 +1,51 @@
 import argparse
 
 
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers()
+def _list(args):
+    _debug(args)
 
-parser_list = subparsers.add_parser('list')
 
-parser_create = subparsers.add_parser('create')
+def _create(args):
+    _debug(args)
 
-parser_edit = subparsers.add_parser('edit')
-parser_edit.add_argument('list')
 
-parser_compare = subparsers.add_parser('compare')
-parser_compare.add_argument('list')
+def _edit(args):
+    _debug(args)
 
-parser_compute = subparsers.add_parser('compute')
-parser_compute.add_argument('list')
+
+def _compare(args):
+    _debug(args)
+
+
+def _compute(args):
+    _debug(args)
+
+
+def _debug(args):
+    print(f"args: {args!r}")
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parser_list = subparsers.add_parser('list')
+    parser_list.set_defaults(func=_list)
+
+    parser_create = subparsers.add_parser('create')
+    parser_create.set_defaults(func=_create)
+
+    parser_edit = subparsers.add_parser('edit')
+    parser_edit.add_argument('list', type=int)
+    parser_edit.set_defaults(func=_edit)
+
+    parser_compare = subparsers.add_parser('compare')
+    parser_compare.add_argument('list', type=int)
+    parser_compare.set_defaults(func=_compare)
+
+    parser_compute = subparsers.add_parser('compute')
+    parser_compute.add_argument('list', type=int)
+    parser_compute.set_defaults(func=_compute)
+
     args = parser.parse_args()
-    print(f"args: {args!r}")
+    args.func(args)
